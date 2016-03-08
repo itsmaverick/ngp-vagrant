b624a2b892b3ad219c1840fa8c53697b9e1c6346
Sadasivudu Malladi <smalladi@infoblox.com>
2016-03-08 12:50:05 -0800

    simplified the script

--- Vagrantfile
+++ Vagrantfile
@@ -1,24 +1,8 @@
 NUM_HOSTS   = 3
-DEV_RAM     = 1024
 HOST_RAM    = 2048
-PUB_NET     = "em1"
 
 Vagrant.configure("2") do |config|
     config.vm.network "public_network"
-
-    config.vm.define "dev" do |dev|
-        config.vm.box = "ubuntu/trusty64"
-        dev.vm.hostname = 'dev'
-        dev.vm.provider :virtualbox do |v|
-            v.customize ["modifyvm", :id, "--memory", DEV_RAM]
-            v.customize ["modifyvm", :id, "--name", "dev"]
-            v.cpus = 1
-        end
-
-        dev.vm.provision "shell" do |s|
-            s.path = "provision/ubuntudevsetup.sh"
-        end
-    end
 
     (1..NUM_HOSTS).each do |i|
         config.vm.define "host#{i}" do |host|
--- provision/ubuntuhostsetup.sh
+++ provision/ubuntuhostsetup.sh
@@ -4,7 +4,7 @@ echo "Provisioning virtual machine..."
 echo "adding gateway..."
 sudo route del -net 0.0.0.0 
 # santa clara your gw is 10.32.0.1, canada:172.31.1.1 add your gateway ip here
-sudo route add -net 0.0.0.0 gw <changeme>  dev eth1
+sudo route add -net 0.0.0.0 gw 10.32.0.1  dev eth1
 
 echo "Installing Docker"
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
@@ -14,6 +14,12 @@ sudo echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt
 
 sudo apt-get update
 sudo apt-get install docker-engine -y > /dev/null
+sudo wget https://get.docker.com/builds/Linux/x86_64/docker-1.9.1
+chmod 755 docker-1.9.1 
+mv docker-1.9.1 /usr/bin/docker
+
+
+
 sudo groupadd docker
 sudo rm -rf /etc/default/docker
 sudo echo 'DOCKER_OPTS="-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock"' > /etc/default/docker
@@ -26,6 +32,10 @@ echo "$new_ip $name $name"
 sed "10 c\
 $new_ip $name $name" /etc/hosts > a
 sudo mv a /etc/hosts
-#sudo echo "$new_ip $name $name" >> /etc/hosts
+
+# Git
+echo "Installing Git"
+sudo apt-get install git -y > /dev/null
+
 echo "Finished provisioning."
 
